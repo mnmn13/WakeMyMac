f084d9e04f4b6885395fbe6f977ce7d1de443201
Nikita Moshyn <nikita.moshyn@gmail.com>
2025-01-20 22:38:55 +0000

    Fix

--- Formula/wakemymac.rb
+++ Formula/wakemymac.rb
@@ -4,6 +4,7 @@ class Wakemymac < Formula
   url "https://github.com/mnmn13/WakeMyMac/releases/download/v1.0.3/WakeMyMac_1.0.3.tar.gz"
   sha256 "6b51ddc396a7ed95826f0fafe3f0049cba67014525c269bd8ce7506fe7d6774d"
   license "MIT"
+  version "0.0.0"
 
   def install
     bin.install "WakeMyMac" => "wake"
